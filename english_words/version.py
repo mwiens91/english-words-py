cf25d094e9c5633a5048cef62461b2f8abd685aa
Matt Wiens <mwiens91@gmail.com>
2022-01-29 01:38:17 -0500

    Use Black for code formatting

--- english_words/version.py
+++ english_words/version.py
@@ -1,5 +1,5 @@
 """Contains name, version, description."""
 
-NAME = 'english-words'
-VERSION = '1.1.0'
+NAME = "english-words"
+VERSION = "1.1.0"
 DESCRIPTION = "contains sets of English words"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from english_words.version import NAME, DESCRIPTION, VERSION
 
 
 # Parse readme to include in PyPI page
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
@@ -13,17 +13,23 @@ setup(
     version=VERSION,
     description=DESCRIPTION.capitalize(),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/mwiens91/english-words-py',
-    author='Matt Wiens',
-    author_email='mwiens91@gmail.com',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/mwiens91/english-words-py",
+    author="Matt Wiens",
+    author_email="mwiens91@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    packages=['english_words'],
+    packages=["english_words"],
     package_data={"english_words": ["web2.txt"]},
 )
