f391e223219bd6131ce2cea4cabd33e5dcf43a10
Matt Wiens <mwiens91@gmail.com>
2025-08-14 14:33:09 -0700

    Fix bad typing hint
    The Iterable type isn't necessarily sized. I could do something like
    Sized & Iterable[str] here but honestly, I'm only passing a list and
    this isn't a general function so there's no need for such an abstract
    type hint.

--- english_words/util.py
+++ english_words/util.py
@@ -1,8 +1,7 @@
 """Stores utility functions for the main function."""
 
-from collections.abc import Iterable
 
-def get_data_file_path(identifier: str, options: Iterable[str]) -> str:
+def get_data_file_name(identifier: str, options: list[str]) -> str:
     # This is basically repeated code taken from the script used to
     # process data
     return (
